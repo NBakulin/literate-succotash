e70ee36dca2d6279a4c8e8e300795a404586e1a9
NBakulin <30417912+NBakulin@users.noreply.github.com>
2017-07-24 23:02:43 +0300

    XSolla one's

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 $requestType = $_SERVER['REQUEST_METHOD']; 
 $query = $_SERVER['QUERY_STRING']; 
 $filename = explode('=',$query)[1]; 
-  switch($requestType) 
+  switch($requestType)
 { 
     case 'HEAD': 
-      header('Filesize: '.filesize($filename)); 
+        header('Filesize: '.filesize($filename)); 
     break; 
     case 'GET': 
-      readfile($filename); 
+        readfile($filename); 
     break; 
     case 'POST': 
-      file_put_contents($filename, file_get_contents('php://input')); 
+        file_put_contents($filename, file_get_contents('php://input')); 
     break; 
     case 'DELETE': 
-     unlink($filename); 
+      unlink($filename); 
     break; 
     case 'PATCH': 
-      $fp = fopen($filename, 'a'); 
-      fwrite ($fp, file_get_contents('php://input')); 
-      fclose($fp); 
+        $fp = fopen($filename, 'a'); 
+        fwrite ($fp, file_get_contents('php://input')); 
+        fclose($fp); 
     break; 
 } 
 ?>
